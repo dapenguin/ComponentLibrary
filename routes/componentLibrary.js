7333dedcbec14a6f1b5cd04821fc08936a424719
dapenguin <anthony.jeffery@littleblueweb.com>
2015-06-15 14:13:10 +0100

    Quick tidy up of comments

--- routes/componentLibrary.js
+++ routes/componentLibrary.js
@@ -3,19 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	// var data = {
-	// 	title: 'Component Library',
-	// 	productData: {
-	// 		name: 'Beer',
-	// 		price: '£1.99',
-	// 		promotions: [
-	// 			'BOGOF',
-	// 			'Save 50p'
-	// 		]
-	// 	}
-	// };
-
 	var data = {
+		title: 'Component Library',
 		components: [
 			{
 				name: 'header',
@@ -23,11 +12,6 @@ router.get('/', function(req, res, next) {
 				partial: 'header',
 				data: {}
 			},
-			// {
-			// 	name: 'buttons',
-			// 	description: 'The different buttons used on the site',
-			// 	partial: 'buttons'
-			// },
 			{
 				name: 'product',
 				description: 'A product',
@@ -43,13 +27,6 @@ router.get('/', function(req, res, next) {
 			}
 		]
 	};
-
-
-	// for each component
-	//   output component name
-	//   output component description
-	//   output component
-	//   output component code
 
 	res.render('pages/componentLibrary', data);
 });
