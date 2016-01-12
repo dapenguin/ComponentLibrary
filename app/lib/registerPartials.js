515df07ffda1b98e224280739ebbabe55d14cd18
dapenguin <anthony.jeffery@littleblueweb.com>
2016-01-12 12:40:06 +0000

    Can now traverse through sub-folders when registering partials

--- app/lib/registerPartials.js
+++ app/lib/registerPartials.js
@@ -1,19 +1,29 @@
 // Helps with this problem:
 // http://stackoverflow.com/questions/8059914/express-js-hbs-module-register-partials-from-hbs-file
 
-module.exports = function(handlebars,dir){
+var registerPartialsInFolder = function(handlebars,dir){
 	var fs = require('fs');
+	var path = require('path');
 
 	var filenames = fs.readdirSync(dir);
 
-	filenames.forEach(function (filename) {
-		var matches = /^([^.]+).hbs$/.exec(filename);
-		if (!matches) {
-			return;
+	filenames.forEach(function(filename){
+		var filePath = path.join(dir, filename),
+			stats = fs.statSync(filePath);
+
+		if (stats.isDirectory()){
+			registerPartialsInFolder(handlebars, filePath);
+		} else {
+			var matches = /^([^.]+).hbs$/.exec(filename);
+			if (!matches) {
+				return;
+			}
+
+			var name = matches[1];
+			var template = fs.readFileSync(filePath, 'utf8');
+			handlebars.registerPartial(name, template, filename);
 		}
-		var name = matches[1];
-		var fullPathToPartial = dir + '/' + filename;
-		var template = fs.readFileSync(fullPathToPartial, 'utf8');
-		handlebars.registerPartial(name, template, filename);
 	});
-}
+};
+
+module.exports = registerPartialsInFolder;
