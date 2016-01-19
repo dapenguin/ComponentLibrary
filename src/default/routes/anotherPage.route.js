e45bf7ce226af8e86fd0deb70755e09f498e0bb9
dapenguin <anthony.jeffery@littleblueweb.com>
2016-01-19 10:52:23 +0000

    Removed dummy file

--- src/default/routes/anotherPage.route.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/somePage', function(req, res, next) {
-	res.send('here is some page');
-});
-
-module.exports = router;
