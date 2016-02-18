b76a12eec75620187b948dacfb66aca0a37a5f6b
dapenguin <anthony.jeffery@littleblueweb.com>
2016-02-18 15:19:50 +0000

    Added helpers for logical comparisons

--- app/helpers/helpers.js
+++ app/helpers/helpers.js
@@ -12,7 +12,7 @@ module.exports = function(hbs){
 
 	hbs.registerHelper('dynamicPartial',function(partialName, context){
 		var partials = hbs.handlebars.partials;
-		
+
 		var partial = partials[partialName];
 
 		var renderPartial = hbs.compile(partial(context));
@@ -26,5 +26,59 @@ module.exports = function(hbs){
 		var html = chartable.lineGraph(array, { width: width, height: height });
 
 		return new hbs.SafeString(html);
+	});
+
+	// Tests whether two values are the same.
+	hbs.registerHelper('eq',function(firstValue, secondValue, options){
+		if (firstValue == secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
+
+	// Tests whether two values are not the same.
+	hbs.registerHelper('ne',function(firstValue, secondValue, options){
+		if (firstValue != secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
+
+	// Tests whether the first value is greater than the second.
+	hbs.registerHelper('gt',function(firstValue, secondValue, options){
+		if (firstValue > secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
+
+	// Tests whether the first value is greater than or equal to the second.
+	hbs.registerHelper('gte',function(firstValue, secondValue, options){
+		if (firstValue >= secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
+
+	// Tests whether the first value is less than the second.
+	hbs.registerHelper('lt',function(firstValue, secondValue, options){
+		if (firstValue < secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
+
+	// Tests whether the first value is less than or equal to the second.
+	hbs.registerHelper('lte',function(firstValue, secondValue, options){
+		if (firstValue <= secondValue){
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
 	});
 };
