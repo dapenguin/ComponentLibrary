1f9d864480b058f9084c50d1ced083bb244630c8
dapenguin <anthony.jeffery@littleblueweb.com>
2016-02-19 12:34:40 +0000

    Added a default page data file as a starting point for all pages

---
+++ src/default/data/pageDefaults.data.js
@@ -0,0 +1,44 @@
+module.exports = {
+	title: 'Welcome to My shop',
+	bodyId: '',
+	bodyClass: '',
+	headerData: {
+		siteName: 'Website name',
+		navItems: [
+			{
+				text: 'Products',
+				href: 'plp'
+			},
+			{
+				text: 'Services',
+				href: '#'
+			},
+			{
+				text: 'About us',
+				href: '#'
+			},
+			{
+				text: 'Contact us',
+				href: '#'
+			},
+			{
+				text: 'FAQ',
+				href: '#'
+			}
+		]
+	},
+	footerData: {
+		links: [
+			{
+				url: 'http://www.twitter.com',
+				linkText: 'Follow us on Twitter',
+				isExternal: true
+			},
+			{
+				url: 'http://www.facebook.com',
+				linkText: 'Like us on Facebook',
+				isExternal: true
+			}
+		]
+	}
+};
